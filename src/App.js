dcb2d59d736056bb0c20fe31df42f519d80bd006
Maurice <mwasmorris777@gmail.com>
2023-11-25 15:21:49 +0300

    Added some code in the App component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Intro from "./components/Intro";
 
 function App() {
+  const [showExpandedMenu, setShowExpandedMenu] = useState(false);
+
   return (
-    <div className="App">
-      <Intro />
+    <div className="text-white bg-black w-full">
+      {/* {!showExpandedMenu && (
+        // We dont want any other component to be mounted on the DOM whenever the menu is open
+        <Navbar setShowExpandedMenu={setShowExpandedMenu} />
+      )} */}
+      {/* <h1>My Portfolio</h1> */}
+      <div className="lg:w-2/3 mx-auto bg-black">
+        {/* {showExpandedMenu && (
+          <ExpandedMenu
+            setShowExpandedMenu={setShowExpandedMenu}
+            setShouldScroll={setShouldScroll}
+            setScrollPosition={setScrollPosition}
+            // We send the scroll position since all components will be unmounted on onmounting expanded menu
+            scrollPosition={scrollPosition}
+          />
+        )} */}
+        {!showExpandedMenu && <Intro />}
+        {/* {!showExpandedMenu && <About />}
+        {!showExpandedMenu && <Projects />}
+        {!showExpandedMenu && <Education />}
+        {!showExpandedMenu && <Footer />} */}
+      </div>
     </div>
   );
 }
