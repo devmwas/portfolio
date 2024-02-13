87aa1810c8d4d902e4f9f172d740f58df2c9a44a
Maurice <mwasmorris777@gmail.com>
2024-02-13 11:30:41 +0300

    Started filling the Skills component with data. Styling and refactoring remains.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,61 @@
 import React from "react";
 
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <div>
+      <div>Skills</div>
+
+      {/* Frontend Development Technologies */}
+      <div>
+        <div className="font-bold text-xl">Frontend Development</div>
+        <div>ReactJS</div>
+        <div>Redux and Redux Toolkit</div>
+        <div>Material UI</div>
+        <div>TailwindCSS</div>
+        <div>Framer Motion</div>
+      </div>
+
+      {/* Backend Development Technologies */}
+      <div>
+        <div className="font-bold text-xl">Backend Development</div>
+        <div>Firebase Realtime Database</div>
+        <div>Firebase Firestore Database</div>
+        <div>Node JS</div>
+        <div>Mongo DB</div>
+        <div>Express</div>
+        <div>Django</div>
+      </div>
+
+      {/* Python Programming Technologies */}
+      <div>
+        <div className="font-bold text-xl">Python Programming</div>
+        <div>
+          <div>Django</div>
+          <div>Pandas</div>
+          <div>NumPy</div>
+          <div>Selenium</div>
+        </div>
+      </div>
+
+      {/* Git and GitHub Development Technologies */}
+      <div className="font-bold text-xl">
+        <div>Git and GitHub Development</div>
+        <div>
+          <div>Deployment of Static Pages to GitHub Pages</div>
+          <div>Software Development Collaboration on GitHub</div>
+        </div>
+      </div>
+
+      {/* Deployment Technologies */}
+      <div className="font-bold text-xl">
+        <div>Deployment</div>
+        <div>
+          <div>Docker</div>
+          <div>Kubernetes</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
