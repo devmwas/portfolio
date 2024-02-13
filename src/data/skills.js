5de87ba39c859978b7b37fd754cdc693ec3c1b59
Maurice <mwasmorris777@gmail.com>
2024-02-13 11:44:20 +0300

    Created a data file for the Skills component to enhance reusability and code maintenance. Now, I'll get started on the reusable Skills component.

---
+++ src/data/skills.js
@@ -0,0 +1,53 @@
+export const skillsData = [
+  // Frontend Development data
+  {
+    name: "Frontend Development",
+    skills: [
+      "ReactJS",
+      "Redux and Redux Toolkit",
+      "Material UI",
+      "TailwindCSS",
+      "Framer Motion",
+    ],
+  },
+
+  //   Backend Development data
+  {
+    name: "Backend Development",
+    skills: [
+      "Firebase Realtime Database",
+      "Firebase Firestore Database",
+      "Node JS",
+      "Mongo DB",
+      "Express",
+      "Django",
+    ],
+  },
+
+  //   Python Programming data
+  {
+    name: "Python Programming",
+    skills: [
+      "Django",
+      "Pandas",
+      "NumPy",
+      "Web scraping and web automation with Selenium",
+    ],
+  },
+
+  //   Git and GitHub
+  {
+    name: "Git and GitHub",
+    skills: [
+      "Deployment of Static Pages to GitHub Pages",
+      "Software Development Collaboration on GitHub",
+      "Contributing to Open Source Software Development",
+    ],
+  },
+
+  //   Software Deployment
+  {
+    name: "Software Deployment",
+    skills: ["Docker", "Kubernetes", "GitHub Pages"],
+  },
+];
